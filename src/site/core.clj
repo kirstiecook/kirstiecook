6c1e6f98658407106caa979fe218585289770f39
Kirstie Cook <kirstiea@gmail.com>
2014-05-15 16:41:22 +0200

    test

--- src/site/core.clj
+++ src/site/core.clj
@@ -4,7 +4,7 @@
 (defn app [req]
   {:status 200
    :headers {"Content-Type" "text/plain"}
-   :body "I'm Kirstie and I'm a software developer.\n\nYou can find me on twitter @krstck and Github @kirstiecook or by email using kc@ this domain.\n\nThis site, humble as it currently is, is completely open source: https://github.com/kirstiecook/kirstiecook"})
+   :body "I'm Kirstie and I'm a software developer.\n\nYou can find me on Github @kirstiecook or by email using kc@ this domain.\n\nThis site, humble as it currently is, is completely open source: https://github.com/kirstiecook/kirstiecook"})
 
 (defn -main [port]
   (jetty/run-jetty app {:port (Integer. port) :join? false}))
